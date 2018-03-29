f53062a55f977fb66b6631c15e94c829af774da7
Bartosz <Bartosz@192.168.1.16>
2018-03-29 23:52:24 +0200

    lab5 do dokonczenia

---
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Ksiegarnia.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl5;
+
+/**
+ *
+ * @author Bartosz
+ */
+public class Ksiegarnia {
+    private String tytul="Wladca Pierscieni";
+    private String autor="Tolkien";
+    private int liczba_stron=1000;
+    public double cena=50.0;
+    Ksiegarnia (double cena)
+    {
+        this.cena=cena;
+    }
+        Ksiegarnia ()
+    {
+
+    }
+    public void pokazDane()
+    {
+        System.out.println("tytul: " + tytul);
+        System.out.println("autor: " + autor);
+        System.out.println("Liczba stron: " + liczba_stron);
+        System.out.println("cena: " + cena);
+    }
+    
+    
+    public String getTytul() {
+        return tytul;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getLiczba_stron() {
+        return liczba_stron;
+    }
+    public double getCena() {
+        return cena;
+    }
+    
+}
--- OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO zad 3, 5, 6
+        Ksiegarnia ksiazka = new Ksiegarnia();
+        System.out.println("Wyswietlanie metoda wszystkich pol klasy: ");
+        ksiazka.pokazDane();
+        ksiazka.cena=60.0;
+        System.out.println("pobieranie wartosci pol za pomoca getterow: ");
+        System.out.println(ksiazka.getTytul());
+        System.out.println(ksiazka.getAutor());
+        System.out.println(ksiazka.getLiczba_stron());
+        System.out.println(ksiazka.getCena());
     }
     
 }
